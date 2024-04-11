0a5fe3db3d8778d4cf89d08905b4091f11e75f24
Tristan-Thompson876 <125337721+Tristan-Thompson876@users.noreply.github.com>
2024-04-11 01:38:39 -0500

    vehicle

--- Management.java
+++ Management.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+
 public class Management {
     private User user;
+    private ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
     public Management(User p){
         this.user = p;
     }
 
     //this method should check if vehicle 
     public void rentVehicle(Date date, Vehicle vehicle){
+
+    }
+
+    //adds a vehicle to arraylist
+    public void addVehicle(Vehicle v){
+
+    }
+
+    public void removeVehicle(Vehicle v){
+        
+    }
+
+    //list all vehicles
+    public void listAllvehicles(){
 
     }
 }
