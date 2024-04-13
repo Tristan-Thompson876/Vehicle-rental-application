796281d24f93dd7862f68b1a01b94fd5f45e33e7
Tristan-Thompson876 <125337721+Tristan-Thompson876@users.noreply.github.com>
2024-04-12 20:04:48 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,17 +1,4 @@
-/*import java.util.ArrayList;
-
-public class Main {
-    ArrayList<User> users;
-    ArrayList<Vehicle> vehicles;
-    
-    public Main() {
-        users = new ArrayList<User>();
-        vehicles = new ArrayList<Vehicle>();
-    }
-    
-    public static void main(String[] args) {
-        Main main = new Main();
-        Management management = new Management(main.users, main.vehicles);
-        LoginScreen log = new LoginScreen(management);
-    }
-}*/
+public static void main(String[] args) {
+    Management management = new Management();
+    new LoginScreen(management);
+}
